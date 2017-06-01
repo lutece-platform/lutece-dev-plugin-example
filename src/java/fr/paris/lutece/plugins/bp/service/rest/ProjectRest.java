14cd2d6b88459238ef180eb4bcc70c34578cc939
seboo <sebleridon@laposte.net>
2017-06-01 11:20:47 +0200

    Update ProjectRest.java
    add the rest plugin basePath
    get the project's hits

--- src/java/fr/paris/lutece/plugins/bp/service/rest/ProjectRest.java
+++ src/java/fr/paris/lutece/plugins/bp/service/rest/ProjectRest.java
@@ -1,108 +1,111 @@
 package fr.paris.lutece.plugins.bp.service.rest;
 
 import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import fr.paris.lutece.plugins.bp.business.Project;
 import fr.paris.lutece.plugins.bp.business.ProjectHome;
+import fr.paris.lutece.plugins.extend.modules.hit.business.Hit;
+import fr.paris.lutece.plugins.rest.service.RestConstants;
+import fr.paris.lutece.plugins.extend.modules.hit.service.HitService;
+import fr.paris.lutece.portal.service.spring.SpringContextService;
 import fr.paris.lutece.plugins.rest.util.json.JSONUtil;
 import fr.paris.lutece.portal.service.message.SiteMessageException;
 
 import net.sf.json.JSONObject;
 
-
-@Path("rest")
+@Path(RestConstants.BASE_PATH + "bp")
 public class ProjectRest {
 
-	@GET
-	@Path("/json/project")	
-	@Produces(MediaType.APPLICATION_JSON)
-	public String getProjects() {
-		
-		Collection<Project> ListeProject =  ProjectHome.getProjectsList();
-		 
-		
-		 JSONObject ListejsonProject = new JSONObject(  );
-		 
-		
-		for (Project project : ListeProject)
-		{
-		
-			 JSONObject json = new JSONObject(  );
-			 json.accumulate( "id", project.getId());
-			 json.accumulate( "name", project.getName());
-			 json.accumulate( "description", project.getDescription());
-			 json.accumulate( "cout", project.getCout());
-			 json.accumulate( "ImageUrl", project.getImageUrl());
-			 
-			 
-			 ListejsonProject.accumulate("project", json);		
-			 
-		}
-		
-		
-			//jsonListeProject.accumulate(Integer.toString(cpt), this.buildJSON(projet));
-			
+    private JSONObject buildJSON(Project project) {
 
-		return ListejsonProject.toString(2);
-		
-	}
-	
+        JSONObject json = new JSONObject();
+        json.accumulate("id", project.getId());
+        json.accumulate("name", project.getName());
+        json.accumulate("description", project.getDescription());
+        json.accumulate("cost", project.getCost());
 
+        return json;
+    }
 
-	
-	private String buildJSON(Project project){
-		String strJSON = "";
-		
-		 JSONObject json = new JSONObject(  );
-		 json.accumulate( "id", project.getId());
-		 json.accumulate( "name", project.getName());
-		 json.accumulate( "description", project.getDescription());
-		 json.accumulate( "cout", project.getCout());
-		 
-		 JSONObject jsonProject = new JSONObject(  );
-		 jsonProject.accumulate( "project", json );
-		 strJSON = jsonProject.toString(2);
-		 
-		 return strJSON;
-	}
+    private JSONObject buildJSONwithStats(Project project) {
+        JSONObject json = new JSONObject();
 
-	@GET
-	@Path("/json/project/{id}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public String getProject(@PathParam("id") int nId) throws SiteMessageException{
+        // get the HitService in the spring context
+        HitService hs = SpringContextService.getBean(HitService.BEAN_SERVICE);
 
-		String strJSON = "";
-		try
-	 {
-		Project project = ProjectHome.findByPrimaryKey(nId);
-		strJSON = this.buildJSON(project);
-	 }
-			        catch ( NumberFormatException e )
-		        {
-		            strJSON = JSONUtil.formatError( "Invalid project number", 3 );
-		        }
-			        catch ( Exception e )
-		        {
-		           strJSON = JSONUtil.formatError( "project not found", 1 );
-	       }
-		
-		return strJSON;
+        String strIdExtendableResource = String.valueOf(project.getId()); // extend resource ID
+        String strExtendableResourceType = Project.PROPERTY_RESOURCE_TYPE; // extend resource type
 
-	}
+        Hit hit;
+        // search nb of hits
+        hit = hs.findByParameters(strIdExtendableResource, strExtendableResourceType);
+
+        json.accumulate("id", project.getId());
+        json.accumulate("name", project.getName());
+        if (hit != null) {
+            json.accumulate("hitNb", hit.getNbHits());
+        }
+
+        return json;
+    }
+
+    @GET
+    @Path("/projects")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getProjectList() {
+
+        Collection<Project> ListeProject = ProjectHome.getProjectsList();
+
+        JSONObject ListejsonProject = new JSONObject();
+
+        for (Project project : ListeProject) {
+            ListejsonProject.accumulate("projects", this.buildJSON(project));
+        }
+
+        return ListejsonProject.toString(2);
+    }
+
+    @GET
+    @Path("/projects/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getProjectById(@PathParam("id") int nId) throws SiteMessageException {
+
+        String strJSON = "";
+        try {
+            Project project = ProjectHome.findByPrimaryKey(nId);
+            strJSON = this.buildJSON(project).toString(2);
+        } catch (NumberFormatException e) {
+            strJSON = JSONUtil.formatError("Invalid project number", 3);
+        } catch (Exception e) {
+            strJSON = JSONUtil.formatError("project not found", 1);
+        }
+
+        return strJSON;
+    }
+
+    @GET
+    @Path("/projects_stats/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getProjectStatsById(@PathParam("id") int nId) throws SiteMessageException {
+
+        String strJSON = "";
+        try {
+            Project project = ProjectHome.findByPrimaryKey(nId);
+            strJSON = this.buildJSONwithStats(project).toString(2);
+        } catch (NumberFormatException e) {
+            strJSON = JSONUtil.formatError("Invalid project number", 3);
+        } catch (Exception e) {
+            strJSON = JSONUtil.formatError("project not found", 1);
+        }
+
+        return strJSON;
+
+    }
 
 //	@PUT
 //	@Path("/json/project/add")
